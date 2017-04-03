d6d9fc95e94e05d20ee1f1aca93fc1cac432b9a7
dennisve <dennis_van_e@hotmail.com>
2017-04-03 12:27:42 +0200

    removeuser uses prepared statements now

--- lib/users/functions/removeUser.php
+++ lib/users/functions/removeUser.php
@@ -9,13 +9,24 @@
 		//prevent sql injection
 		$rnummer = mysqli_real_escape_string($dal->getConn(), $rnummer);
 
-		//delete user from DB
-		$sql = "DELETE FROM gebruiker WHERE rnummer = '".$rnummer."'";
-		$dal->writeDB($sql);
+		//create array of parameters
+		//first item = parameter types
+		//i = integer
+		//d = double
+		//b = blob
+		//s = string
+		$parameters[0] = "s";
+		$parameters[1] = $rnummer;
 
+		//prepare statement
+		//delete user from DB
+		$dal->setStatement("DELETE FROM gebruiker WHERE rnummer = ?");
+		$dal->writeDB($parameters);
+
+		//prepare statement
 		//delete user from projects -- also possible to delete from bestellingen, but probably not for the best
-		$sql = "DELETE FROM gebruikerproject WHERE rnummer = '".$rnummer."'";
-		$dal->writeDB($sql);
+		$dal->setStatement("DELETE FROM gebruikerproject WHERE rnummer = ?");
+		$dal->writeDB($parameters);
 
 		//close the connection
 		$dal->closeConn();
