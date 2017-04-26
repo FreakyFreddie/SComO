d896cd45a1af60a77869bb41280b6e138d142d49
Werefoxx <mommensjelle@hotmail.be>
2017-04-26 19:01:03 +0200

    Dashboard
    Added total price for components ordered

--- web/AJAX/adminChartDataRequest.php
+++ web/AJAX/adminChartDataRequest.php
@@ -49,6 +49,7 @@
 
 		//create output array: milestone date + total amount for that week
 		$output = array();
+        $output2 = array();
 
 		//prepare the milestone dates for output
 		foreach($dates as $key => $date)
@@ -65,6 +66,14 @@
 			GROUP BY datum
 			ORDER BY datum DESC";
 		$records = $dal->queryDBNoArgs($sql);
+
+        $sql2 = "SELECT CAST(bestelling.besteldatum AS DATE) AS datum2, SUM((bestellingproduct.aantal * bestellingproduct.prijs)) AS totaal
+			FROM bestelling
+			INNER JOIN bestellingproduct
+			ON bestelling.bestelnummer=bestellingproduct.bestelnummer
+			GROUP BY datum2
+			ORDER BY datum2 DESC";
+        $records2 = $dal->queryDBNoArgs($sql2);
 		$dal->closeConn();
 
 		//start a counter for the array
@@ -135,6 +144,67 @@
 		//reverse the output
 		$output = array_reverse($output);
 
+            //restart a counter for the array
+        $i = 0;
+
+        //start counting at 0
+        $totalweekprice = 0;
+
+        //we need to print the amount spent this week
+
+        //every week (e.g. count 7 days back from today, count 7 days back from that date) is a milestone date
+
+        foreach($records2 as $key => $record2)
+        {
+            //exit the loop if we go over the max date
+            if(date_diff($dates[$i],date_create($record2->datum2))->days > ($scale*$counter*7))
+            {
+                break;
+            }
+
+            //if the date is within one scale unit from a milestone date add the amount corresponding the date to the total for this week
+            //if there is more than one scale unit between the milestone date & amount date, keep checking
+            if(date_diff($dates[$i],date_create($record2->datum2))->days > $scale*7)
+            {
+                //security loop in case there was a week with 0 orders
+                do
+                {
+                    //write amount for this week to array
+                    $output2[$i]["amount"] = $totalweekprice;
+
+                    //reset totalweekprice back to 0
+                    $totalweekprice = 0;
+
+                    //counter goes up
+                    $i++;
+                }
+                while((date_diff($dates[$i],date_create($record2->datum2))->days > $scale*7));
+
+                $totalweekprice = $totalweekprice + $record2->totaal;
+            }
+            else
+            {
+                $totalweekprice = $totalweekprice + $record2->totaal;
+            }
+        }
+
+        //write amount for this week to array
+        $output2[$i]["amount"] = $totalweekprice;
+
+        //if counter has not reached the max yet, set all previous dates with amount 0
+        if($i < 11)
+        {
+            $i++;
+            for($i; $i <=11; $i++)
+            {
+                //write amount for this week to array
+                $output2[$i]["amount"] = 0;
+            }
+        }
+
+        //reverse the output
+        $output2 = array_reverse($output2);
+
 		//print the info to create a JSON object with javascript
 		echo '{"labels" : ["'.$output[0]["date"].'","'.$output[1]["date"].'","'.$output[2]["date"].'","'.$output[3]["date"].'","'.$output[4]["date"].'","'.$output[5]["date"].'","'.$output[6]["date"].'","'.$output[7]["date"].'","'.$output[8]["date"].'","'.$output[9]["date"].'","'.$output[10]["date"].'","'.$output[11]["date"].'"],
 				"datasets" : [
@@ -147,7 +217,18 @@
 						"pointHighlightFill" : "#fff",
 						"pointHighlightStroke" : "rgba(48, 164, 255, 1)",
 						"data" : ['.$output[0]["amount"].','.$output[1]["amount"].','.$output[2]["amount"].','.$output[3]["amount"].','.$output[4]["amount"].','.$output[5]["amount"].','.$output[6]["amount"].','.$output[7]["amount"].','.$output[8]["amount"].','.$output[9]["amount"].','.$output[10]["amount"].','.$output[11]["amount"].']
+					},
+					{
+						"label": "My Second dataset",
+						"fillColor" : "rgba(114, 114, 140, 0.2)",
+						"strokeColor" : "rgba(114, 114, 140, 1)",
+						"pointColor" : "rgba(114, 114, 140, 1)",
+						"pointStrokeColor" : "#fff",
+						"pointHighlightFill" : "#fff",
+						"pointHighlightStroke" : "rgba(114, 114, 140, 1)",
+						"data" : ['.$output2[0]["amount"].','.$output2[1]["amount"].','.$output2[2]["amount"].','.$output2[3]["amount"].','.$output2[4]["amount"].','.$output2[5]["amount"].','.$output2[6]["amount"].','.$output2[7]["amount"].','.$output2[8]["amount"].','.$output2[9]["amount"].','.$output2[10]["amount"].','.$output2[11]["amount"].']
 					}
 				]}';
+
 	}
 ?>
